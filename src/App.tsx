acc2cb332a887e70c2abf70b9bfe2f8401d85841
PLKuba <major.plays96@gmail.com>
2023-12-28 23:48:42 +0100

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
@@ -9,77 +9,77 @@ function App() {
   const [timerMinutes, setTimerMinutes] = useState("00");
   const [timerSeconds, setTimerSeconds] = useState("00");
 
-  let interval = useRef();
-
   const startTimer = () => {
-    const countDownDate = new Date("December 28 2023 23:04:00").getTime();
-    interval = setInterval(() => {
+    const countDownDate = new Date("December 29 2023 23:04:00").getTime();
+
+    const interval = setInterval(() => {
       const now = new Date().getTime();
       const distance = countDownDate - now;
 
-      const days = Math.floor((distance / (1000 * 60 * 60 * 24)));
-      const hours = Math.floor(((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-      const minutes = Math.floor(((distance % (1000 * 60 * 60)) / (1000 * 60)));
-      const seconds = Math.floor(((distance % (1000 * 60)) / 1000));
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       if (distance < 0) {
-        clearInterval(interval.current);
+        clearInterval(interval);
       } else {
-        console.log(seconds);
-        setTimerDays(days);
-        setTimerHours(hours);
-        setTimerMinutes(minutes);
-        setTimerSeconds(seconds);
+        setTimerDays(days.toString());
+        setTimerHours(hours.toString());
+        setTimerMinutes(minutes.toString());
+        setTimerSeconds(seconds.toString());
       }
     }, 1000);
+
+    return interval;
   };
 
   useEffect(() => {
-    startTimer();
+    const interval = startTimer();
     return () => {
-      clearInterval(interval.current);
+      clearInterval(interval);
     };
-  });
+  }, []);
 
   return (
-    <section className="timer-container">
-      <section className="timer">
-        <div>
-          <span className="timer-icon bi bi-calendar-date"></span>
-          <h2>Countdown timer</h2>
-          <p>Countdown to a really special event</p>
-        </div>
-        <div>
-          <section>
-            <p>{timerDays}</p>
-            <p>
-              <small>Days</small>
-            </p>
-          </section>
-          <span>:</span>
-          <section>
-            <p>{timerHours}</p>
-            <p>
-              <small>Hours</small>
-            </p>
-          </section>
-          <span>:</span>
-          <section>
-            <p>{timerMinutes}</p>
-            <p>
-              <small>Minutes</small>
-            </p>
-          </section>
-          <span>:</span>
-          <section>
-            <p>{timerSeconds}</p>
-            <p>
-              <small>Seconds</small>
-            </p>
-          </section>
-        </div>
+      <section className="timer-container">
+        <section className="timer">
+          <div>
+            <span className="timer-icon bi bi-calendar-date"></span>
+            <h2>Countdown timer</h2>
+            <p>Countdown to a really special event</p>
+          </div>
+          <div>
+            <section>
+              <p>{timerDays}</p>
+              <p>
+                <small>Days</small>
+              </p>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerHours}</p>
+              <p>
+                <small>Hours</small>
+              </p>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerMinutes}</p>
+              <p>
+                <small>Minutes</small>
+              </p>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerSeconds}</p>
+              <p>
+                <small>Seconds</small>
+              </p>
+            </section>
+          </div>
+        </section>
       </section>
-    </section>
   );
 }
 
